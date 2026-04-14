6dfe76ac390f1351e94c357fbd4a09ee590d3436
Dark-004 <martinbiju04@gmail.com>
2026-04-14 23:02:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const cover = document.getElementById("cover");
+const card = document.getElementById("card");
+
+cover.addEventListener("click", () => {
+  cover.classList.add("open");
+
+  setTimeout(() => {
+    cover.style.display = "none";
+    card.style.display = "block";
+  }, 1000);
+});
